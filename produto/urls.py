fc8a99e4f8616b33fd23ed75c8e5bff41073fab1
Caiua <caiua777@gmail.com>
2026-02-21 10:55:43 -0300

    Detalhes do Produto

--- produto/urls.py
+++ produto/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.ListaProdutos.as_view(), name='lista'),
     path('<slug>', views.DetalheProduto.as_view(), name='detalhe'),
     path('adicionaraocarrinho/', views.AdicionarAoCarrinho.as_view(), name='adicionaraocarrinho'),
+    path('carrinho/', views.Carrinho.as_view(), name='carrinho'),
     path('finalizar/', views.Finalizar.as_view(), name='finalizar'),
-
 ]
